f57c35c7830edf50e68ad12c20ff9158389c551d
coffeetron832 <jpontonh@gmail.com>
2025-05-27 10:32:16 -0500

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = process.env.JWT_SECRET;
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
+
   if (!token) return res.status(401).json({ error: 'Token no proporcionado' });
 
-  jwt.verify(token, JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(403).json({ error: 'Token inválido' });
     req.user = user;
     next();
@@ -14,3 +14,4 @@ function authenticateToken(req, res, next) {
 }
 
 module.exports = { authenticateToken };
+
