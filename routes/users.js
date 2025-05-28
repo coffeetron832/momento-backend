e35247ec8fd3043cd92b61695ee3baad482e01ae
coffeetron832 <jpontonh@gmail.com>
2025-05-27 22:25:42 -0500

    Delete routes directory

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticateToken } = require('../middleware/authMiddleware');
-
-router.get('/profile', authenticateToken, (req, res) => {
-  res.json({ message: 'Perfil accedido correctamente', user: req.user });
-});
-
-module.exports = router;
