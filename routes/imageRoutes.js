e16100dfedbc5e63ff21cb143e880005a699d577
coffeetron832 <jpontonh@gmail.com>
2025-05-28 10:17:08 -0500

    Update imageRoutes.js

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,9 +1,11 @@
+// routes/imageRoutes.js
 const express = require('express');
 const router = express.Router();
 const { uploadImage, getImages } = require('../controllers/imageController');
 const auth = require('../middleware/authMiddleware');
+const upload = require('../middleware/upload');
 
-router.post('/upload', auth, uploadImage);
+router.post('/upload', auth, upload.single('image'), uploadImage);
 router.get('/', getImages); // públicas
 
 module.exports = router;
