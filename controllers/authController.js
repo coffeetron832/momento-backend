da530fa98bd2e21c8fb5da04d8112696525b578a
coffeetron832 <jpontonh@gmail.com>
2025-05-27 22:31:28 -0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,32 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const createToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '2h' });
+};
+
+exports.register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.create({ username, password });
+    const token = createToken(user._id);
+    res.status(201).json({ token });
+  } catch (err) {
+    res.status(400).json({ error: 'Usuario ya existe o datos inválidos' });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(401).json({ error: 'Credenciales incorrectas' });
+    }
+    const token = createToken(user._id);
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+};
 
