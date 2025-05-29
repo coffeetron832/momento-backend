dd0face61cf6f715e0360dc7ccacd5500f98a024
coffeetron832 <jpontonh@gmail.com>
2025-05-29 10:59:06 -0500

    Update Image.js

--- models/Image.js
+++ models/Image.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const imageSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   imageUrl: { type: String, required: true },
+  publicId: { type: String, required: true }, // 👈 NUEVO CAMPO para eliminar desde Cloudinary
   description: { type: String, maxlength: 120 },
   createdAt: { type: Date, default: Date.now, expires: 21600 } // 6 horas en segundos
 });
 
 module.exports = mongoose.model('Image', imageSchema);
+
